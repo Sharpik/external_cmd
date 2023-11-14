b0302b2bf1188dfd7d952e062b09a91795ab8671
Sharp <3375029+Sharpik@users.noreply.github.com>
2023-11-14 21:25:18 +0100

    fix

--- init.lua
+++ init.lua
@@ -5,24 +5,26 @@
 
 -- Put the external commands to "../world/external_cmd/commands_input.txt"
 
-local repeat_after = minetest.settings:get("external_cmd_interval") or 2 -- repeat every x seconds
-local report_back = minetest.settings:get("external_cmd_log") or false -- make commands_output.csv with commands log
-local show_notification = minetest.settings:get("external_cmd_notifi") or false -- do you want to see notification that command was executed in game?
+local repeat_after = tonumber(minetest.settings:get("external_cmd_interval")) or 2 -- repeat every x seconds
+local report_back = minetest.settings:get_bool("external_cmd_log", true) -- make commands_output.csv with commands log
+local show_notification = minetest.settings:get_bool("external_cmd_notifi", false) -- do you want to see notification that command was executed in game?
 
 -- register this user first at your server and give it full permissions (/setpassword SERVER <secretpass> && /grant SERVER all)
 local admin = "SERVER"
 
+local world_dir = minetest.get_worldpath("external_cmd")
+minetest.mkdir(world_dir.."/external_cmd/")
+
 local function cron()
 	local dtime = minetest.get_gametime()
-	local world_dir = minetest.get_worldpath("external_cmd")
 	local file_input = "commands_input.txt"
 	local file_output = "commands_output.csv"
 	local message_type = "command"
 	
-    local file_in = (io.open(world_dir.."/external_cmd/"..file_input, "r"))
-		if file_in ~= nil then
+	local file_in = (io.open(world_dir.."/external_cmd/"..file_input, "r"))
+		if (file_in ~= nil) then
 			for command in file_in:lines() do
-				if command ~= nil then
+				if (command ~= nil) then
 					local cmd, param = string.match(command, "^/([^ ]+) *(.*)")
 					if not param then
 						param = ""
@@ -30,7 +32,7 @@ local function cron()
 					
 					local cmd_obj = minetest.chatcommands[cmd]
 					if cmd_obj then
-						if show_notification then
+						if (show_notification == true) then
 							minetest.log("external_cmd: "..dtime.." Issued command /"..cmd.." "..param)
 						end
 						cmd_obj.func(admin, param)
@@ -39,9 +41,8 @@ local function cron()
 						message_type = "message"
 					end
 					
-					if report_back then
+					if (report_back == true) then
 						-- Report it back to external file
-						minetest.mkdir(world_dir.."/external_cmd/")
 						local file_out = (io.open(world_dir.."/external_cmd/"..file_output, "a"))
 						if file_out ~= nil then
 							file_out:write(dtime..";"..admin..";"..message_type..";"..command,'\n')
@@ -57,4 +58,20 @@ local function cron()
 	-- repeat that function after the time interval
 	minetest.after(repeat_after, cron)
 end
+
+local function check_priv()
+	if minetest.check_player_privs(admin, "server") then
+		return true
+	else
+		minetest.log("external_cmd: Don't have permissions set! Set permissions and restart the server.")
+		return false
+	end
+end
+
+--[[
+if check_priv() then
+	minetest.after(repeat_after, cron)
+end
+--]]
+
 minetest.after(repeat_after, cron)
